816f610809767c3426e6b5225efb0edd6d95f340
Fitra Zul Fahmi <fitradzulfahmi@gmail.com>
2019-09-25 16:05:11 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import React from 'react';
 import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import './App.css';
+
+import Timer from './pages/Timer';
+import Home from './pages/Home';
+import Point from './pages/Point';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/timer/">Timer</Link>
+            </li>
+            <li>
+              <Link to="/point/">Point</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Route path="/" exact component={Home} />
+        <Route path="/timer/" component={Timer} />
+        <Route path="/point/" component={Point} />
+      </div>
+    </Router>
   );
 }
 
---
+++ src/pages/Point.js
@@ -0,0 +1,164 @@
+import React from 'react';
+import { Container, Row, Col, Card, CardTitle, CardText,
+  Form, FormGroup, Input, Button } from 'reactstrap';
+
+class Point extends React.Component {
+  state = {
+    point1: 0,
+    point2: 0,
+    point3: 0,
+    point4: 0,
+    val1: 0,
+    val2: 0,
+    val3: 0,
+    val4: 0
+  }  
+  decrement = (x) => {
+    if (x === 1 || x === "1"){
+      let val = this.state.point1 - parseInt(this.state.val1);
+      this.setState({point1: val});  
+    } else if (x === 2 || x === "2"){
+      let val = this.state.point2 - parseInt(this.state.val2);
+      this.setState({point2: val});
+    } else if (x === 3 || x === "3") {
+      let val = this.state.point3 - parseInt(this.state.val3);
+      this.setState({point3: val});  
+    } else if (x === 4 || x === "4") {
+      let val = this.state.point4 - parseInt(this.state.val4);
+      this.setState({point4: val});
+    }
+  }  
+
+  increment = (x) => {
+    switch (x) {
+      case 1:
+        let val = this.state.point1 + parseInt(this.state.val1);
+        this.setState({point1: val});
+        break;
+      case 2:
+        let val2 = this.state.point2 + parseInt(this.state.val2);
+        this.setState({point2: val2});
+        break;
+      case 3:
+        let val3 = this.state.point3 + parseInt(this.state.val3);
+        this.setState({point3: val3});
+        break;
+      case 4:
+        let val4 = this.state.point4 + parseInt(this.state.val4);
+        this.setState({point4: val4});
+        break;
+      default:
+        break;
+    }  
+  }  
+
+  onChangeVal1 = (e) => {
+    e.preventDefault();
+    this.setState({val1: e.target.value})
+  }
+
+  onChangeVal2 = (e) => {
+    e.preventDefault();
+    this.setState({val2: e.target.value})
+  }
+
+  onChangeVal3 = (e) => {
+    e.preventDefault();
+    this.setState({val3: e.target.value})
+  }
+
+  onChangeVal4 = (e) => {
+    e.preventDefault();
+    this.setState({val4: e.target.value})
+  }
+
+  render() {
+    return (
+    <>
+      <Container className="bg-secondary" fluid>
+        <Row className="my-2">
+          <Col xl="3" lg="3" sm="3" className="my-3">
+            <Card body>
+              <CardTitle><p className="text-center h2">ABU BAKAR</p></CardTitle>
+              <CardText><p className="text-center display-1">{this.state.point1}</p></CardText>
+            </Card>
+          </Col>
+          <Col xl="3" lg="3" sm="3" className="my-3">
+            <Card body>
+              <CardTitle><p className="text-center h2">UMAR</p></CardTitle>
+              <CardText><p className="text-center display-1">{this.state.point2}</p></CardText>
+            </Card>
+          </Col>
+          <Col xl="3" lg="3" sm="3" className="my-3">
+            <Card body>
+              <CardTitle><p className="text-center h2">USMAN</p></CardTitle>
+              <CardText><p className="text-center display-1">{this.state.point3}</p></CardText>
+            </Card>
+          </Col>
+          <Col xl="3" lg="3" sm="3" className="my-3">
+            <Card body>
+              <CardTitle><p className="text-center h2">ALI</p></CardTitle>
+              <CardText><p className="text-center display-1">{this.state.point4}</p></CardText>
+            </Card>
+          </Col>
+        </Row>  
+        <Row>
+          <Col xl="3" lg="3" sm="3" className="my-3 text-center">
+            <Row>
+              <Col sm={2}>
+                <Button color="danger" className="h1" type="button" onClick={() => this.decrement(1)}>-</Button>
+              </Col>
+              <Col sm={6}>
+                <Input  type="text" name="group1" id="group1" placeholder="0" value={this.state.val1} onChange={this.onChangeVal1} />
+              </Col>
+              <Col sm={2}>
+                <Button color="success" className="h1" type="button" onClick={() => this.increment(1)}>+</Button>
+              </Col>
+            </Row>
+          </Col>  
+          <Col xl={3} lg={3} sm={3} className="my-3 text-center">
+            <Row>
+              <Col sm={2}>
+                <Button color="danger" className="h1" type="button" onClick={()=>this.decrement(2)}>-</Button>
+              </Col>
+              <Col sm={6}>
+                <Input  type="text" name="group2" id="group2" placeholder="0" value={this.state.val2} onChange={this.onChangeVal2} />
+              </Col>
+              <Col sm={2}>
+                <Button color="success" className="h1" type="button" onClick={()=>this.increment(2)}>+</Button>
+              </Col>
+            </Row>
+          </Col>  
+          <Col xl={3} lg={3} sm={3} className="my-3 text-center">
+            <Row>
+              <Col sm={2}>
+                <Button color="danger" className="h1" type="button" onClick={()=>this.decrement(3)}>-</Button>
+              </Col>
+              <Col sm={6}>
+                <Input  type="text" name="group3" id="group3" placeholder="0" value={this.state.val3} onChange={this.onChangeVal3} />
+              </Col>
+              <Col sm={2}>
+                <Button color="success" className="h1" type="button" onClick={()=>this.increment(3)}>+</Button>
+              </Col>
+            </Row>
+          </Col>  
+          <Col xl={3} lg={3} sm={3} className="my-3 text-center">
+            <Row>
+              <Col sm={2}>
+                <Button color="danger" className="h1" type="button" onClick={()=>this.decrement(4)}>-</Button>
+              </Col>
+              <Col sm={6}>
+                <Input  type="text" name="group4" id="group4" placeholder="0" value={this.state.val4} onChange={this.onChangeVal4} />
+              </Col>
+              <Col sm={2}>
+                <Button color="success" className="h1" type="button" onClick={()=>this.increment(4)}>+</Button>
+              </Col>
+            </Row>
+          </Col>  
+        </Row>
+      </Container></>
+    )
+  }  
+}
+
+export default Point;
